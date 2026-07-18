894dc29e2189c96580387818908310d793a478dd
tetsu <tetsu@example.com>
2026-07-18 22:34:30 +0900

    chore: refresh widened card cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'free-slots-v11';
+const CACHE_NAME = 'free-slots-v12';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
