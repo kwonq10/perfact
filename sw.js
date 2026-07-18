75e45ba9e0402f8bff74ea3c2bd8920c0e1992a3
kwonq10 <163816953+kwonq10@users.noreply.github.com>
2026-07-18 14:51:24 +0900

    chore: refresh next-week swipe cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'free-slots-v5';
+const CACHE_NAME = 'free-slots-v6';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
