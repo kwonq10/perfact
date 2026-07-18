4c968fa524e92b9465091550724326e4ee7c0683
tetsu <tetsu@example.com>
2026-07-18 22:55:14 +0900

    chore: refresh available time label cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'free-slots-v13';
+const CACHE_NAME = 'free-slots-v14';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
